7a0a5823e642e2cb3bce59d9a718688cd1cf33cd
Saurabh Dammani <saurabhdammani@gmail.com>
2019-12-01 00:24:10 +0530

    Committing in develop-1.1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 //Comment from develop-1.1
+//Got the comment
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
